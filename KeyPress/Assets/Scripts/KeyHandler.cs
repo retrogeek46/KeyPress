e1a61e3da4e9ff4125761591fee61b9c5af2af1e
retrogeek46 <divyangpradeep@gmial.com>
2019-03-10 01:42:06 +0530

    Adding project files

---
+++ KeyPress/Assets/Scripts/KeyHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyHandler : MonoBehaviour {
+
+    //passed as argument in function
+    public GameObject[] keysSetter;
+    //setter for above array
+    public static GameObject[] keys; 
+    //string
+    private static string[] keyNames = {"Oem3", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D0", "OemMinus", "Oemplus", "Back",
+                                        "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "Oem4", "Oem6",
+                                        "CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", "OemSemicolon", "Oem7", "OemPipe", "Return",
+                                        "LShiftKey", "Z", "X", "C", "V", "B", "N", "M", "Oemcomma", "OemPeriod", "Oem2", "RShiftKey",
+                                        "LControlKey", "LWin", "LMenu", "Space", "RMenu", "RWin", "Apps", "RControlKey",
+                                        "Up", "Left", "Down", "Right",
+                                        "Insert", "Home", "PageUp", "Delete", "End", "PageDown" };
+
+    // Use this for initialization
+    void Awake() {
+        //initialize static gameobject array from inspector values
+        keys = keysSetter;
+    }
+    
+    //change color of key in scene based on key press
+    static void ChangeKeyColor(GameObject key, bool pressed) {
+        if (pressed) {
+            key.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        } else if (!pressed) {
+            key.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        
+    }
+    
+    //Takes string argument and calls PressKey
+    public static void ChangeKey(string searchString, bool pressed) {
+        //arrows
+        int index = Array.IndexOf(keyNames, searchString);
+        if (index > -1) {
+            ChangeKeyColor(keys[index], pressed);
+        } 
+    }
+}
---
+++ KeyPress/Assets/Scripts/Keylogger.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+using UnityEngine;
+
+public class Keylogger : MonoBehaviour {
+
+	private static int WH_KEYBOARD_LL = 13;
+	private static int WM_KEYDOWN = 0x0100;
+	private static int WM_KEYUP = 0x0101;
+	private static int WM_SYSKEYDOWN = 0x0104;
+	private static int WM_SYSKEYUP = 0x0105;
+	private static IntPtr hook = IntPtr.Zero;
+	private static LowLevelKeyboardProc llkProcedure = HookCallback;
+
+	// Use this for initialization
+	void Start () {
+		hook = SetHook(llkProcedure);
+	}
+
+	private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+	private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+		//regular keys
+		if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
+			int vkCode = Marshal.ReadInt32(lParam);
+			KeyHandler.ChangeKey(((Keys)vkCode).ToString(), true);
+		}
+		if (nCode >= 0 && wParam == (IntPtr)WM_KEYUP) {
+			int vkCode = Marshal.ReadInt32(lParam);
+			KeyHandler.ChangeKey(((Keys)vkCode).ToString(), false);
+		}
+		//alt key
+		if (nCode >= 0 && wParam == (IntPtr)WM_SYSKEYDOWN) {
+			int vkCode = Marshal.ReadInt32(lParam);
+			KeyHandler.ChangeKey(((Keys)vkCode).ToString(), true);
+		}
+		if (nCode >= 0 && wParam == (IntPtr)WM_SYSKEYUP) {
+			int vkCode = Marshal.ReadInt32(lParam);
+			KeyHandler.ChangeKey(((Keys)vkCode).ToString(), false);
+		}
+		return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+	}
+
+	private static IntPtr SetHook(LowLevelKeyboardProc proc) {
+		Process currentProcess = Process.GetCurrentProcess();
+		ProcessModule currentModule = currentProcess.MainModule;
+		string moduleName = currentModule.ModuleName;
+		IntPtr moduleHandle = GetModuleHandle(moduleName);
+		return SetWindowsHookEx(WH_KEYBOARD_LL, llkProcedure, moduleHandle, 0);
+	}
+
+	[DllImport("user32.dll")]
+	private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+	[DllImport("user32.dll")]
+	private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+	[DllImport("user32.dll")]
+	private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+	[DllImport("kernel32.dll")]
+	private static extern IntPtr GetModuleHandle(String lpModuleName);
+
+	private void OnDestroy() {
+		Unhook();
+	}
+
+	public void Unhook() {
+		UnhookWindowsHookEx(hook);
+	}
+}
+
+
